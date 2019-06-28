1f8bb454b5bafdf6b8fa8e981c3a3e9603996a64
Plutonium <seksariamanav.2000@gmail.com>
2019-06-28 13:53:41 +0530

    Update Developers.vue

--- src/views/Developers.vue
+++ src/views/Developers.vue
@@ -1,74 +1,76 @@
 <template>
-    <div class="developers">
-             <v-container fluid class="outside">
-    <v-layout row wrap>
-      <v-flex xs12>
-        <h1 class="heading-1">DEVELOPERS</h1>
-      </v-flex>
-    </v-layout>
-<v-layout row wrap justify-center align-center class="row">
-      <v-flex sm4 xs12 class="contact">
-        <v-card class="card" elevation="5" color="rgba(0,0,0,0.75)" dark>
-          <div class="img-1">
-            <v-avatar size="175" class="img-container">
-              <img
-                src="https://firebasestorage.googleapis.com/v0/b/waves-assets.appspot.com/o/Bharat.jpeg?alt=media&token=b4bbf164-882d-43ea-a9ad-05cd72207bcf"
-                class="image"
-              >
-            </v-avatar>
-          </div>
-
-          <div class="details">
-            <h3>Bharat Dhir</h3>
-            <h4>Events Head</h4>
-            <h4>8427384030</h4>
-            <h4>bharatdhir@bitswaves.org</h4>
-          </div>
-        </v-card>
-      </v-flex>
-
-      <v-flex sm4 xs12 class="contact">
-        <v-card class="card" elevation="5" color="rgba(0,0,0,0.75)" dark>
-          <div class="img-1">
-            <v-avatar size="175" class="img-container">
-              <img
-                src="https://firebasestorage.googleapis.com/v0/b/waves-assets.appspot.com/o/Sanyukta_1.jpg?alt=media&token=a0b09fe2-a2df-44b6-a50d-e3db7726a24a"
-                class="image"
-              >
-            </v-avatar>
-          </div>
-
-          <div class="details">
-            <h3>Sanyukta Jain</h3>
-            <h4>Chief Coordinator</h4>
-            <h4>9990931969</h4>
-            <h4>sanyukta@bitswaves.org</h4>
-          </div>
-        </v-card>
-      </v-flex>
-
-      <v-flex sm4 xs12 class="contact">
-        <v-card class="card" elevation="5" color="rgba(0,0,0,0.75)" dark>
-          <div class="img-1">
-            <v-avatar size="175" class="img-container">
-              <img
-                src="https://firebasestorage.googleapis.com/v0/b/waves-assets.appspot.com/o/Kratik.jpeg?alt=media&token=ce9f2c52-ec0b-43ba-9925-d262817847ec"
-                class="image"
-              >
-            </v-avatar>
-          </div>
-
-          <div class="details">
-            <h3>Kratik Lodha</h3>
-            <h4>Operations Head</h4>
-            <h4>8275145404</h4>
-            <h4>kratik@bitswaves.org</h4>
-          </div>
-        </v-card>
-      </v-flex>
-    </v-layout>
-        </v-container>
+  <div class="container">
+    <h1>Profile Card</h1>
+    <div class="row">
+        <div class="col-sm-6 col-md-4">
+            <div class="profile-card">
+                <div class="profile-img">
+                    <img src="https://static.pexels.com/photos/14661/pexels-photo-14661.jpeg" alt="Team Image"/>
+                </div>
+                <div class="profile-content">
+                    <h2 class="title">RAJATH REGHUNATH
+                        <span>Head Developer</span>
+                        <span>rrgodhorus@gmail.com</span>
+                    </h2>
+                    <ul class="social-link">
+                        <li><a href="https://github.com/rrgodhorus"><i class="fab fa-github"></i></a></li> &emsp;
+                        <li><a href="https://www.linkedin.com/in/rajath-reghunath-4180b8163"><i class="fab fa-linkedin-in"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6 col-md-4">
+            <div class="profile-card">
+                <div class="profile-img">
+                    <img src="https://static.pexels.com/photos/14661/pexels-photo-14661.jpeg" alt="Team Image"/>
+                </div>
+                <div class="profile-content">
+                    <h2 class="title">Manav Seksaria
+                        <span>Designer, Developer</span>
+                        <span>seksariamanav.2000@gmail.com</span>
+                    </h2>
+                    <ul class="social-link">
+                        <li><a href="https://github.com/rrgodhorus"><i class="fab fa-github"></i></a></li>&emsp;
+                        <li><a href="https://www.linkedin.com/in/rajath-reghunath-4180b8163"><i class="fab fa-linkedin-in"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6 col-md-4">
+            <div class="profile-card">
+                <div class="profile-img">
+                    <img src="https://static.pexels.com/photos/14661/pexels-photo-14661.jpeg" alt="Team Image"/>
+                </div>
+                <div class="profile-content">
+                    <h2 class="title">Nikhil Boddupalli
+                        <span>Designer,Developer</span>
+                        <span>nikhil2001.12.5@gmail.com</span>
+                    </h2>
+                    <ul class="social-link">
+                        <li><a href="https://github.com/f20180106"><i class="fab fa-github"></i></a></li>&emsp;
+                        <li><a href="https://www.linkedin.com/in/nikhil-boddupalli-504257129/"><i class="fab fa-linkedin-in"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6 col-md-4">
+            <div class="profile-card">
+                <div class="profile-img">
+                    <img src="https://static.pexels.com/photos/14661/pexels-photo-14661.jpeg" alt="Team Image"/>
+                </div>
+                <div class="profile-content">
+                    <h2 class="title">Henry Polles
+                        <span>Designer,Developer</span>
+                    </h2>
+                    <ul class="social-link">
+                        <li><a href="https://github.com/rrgodhorus"><i class="fab fa-github"></i></a></li>&emsp;
+                        <li><a href="https://www.linkedin.com/in/rajath-reghunath-4180b8163"><i class="fab fa-linkedin-in"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     </div>
+   </div>
 </template>
 
 
@@ -85,81 +87,143 @@ export default {
     height:100%;
     background-image: url("https://firebasestorage.googleapis.com/v0/b/waves-assets.appspot.com/o/Backgrounds%2FBackground_Events.jpg?alt=media&token=d2a2bd28-36f6-43de-825c-8de42b473ed8");
 }
-@import url("https://fonts.googleapis.com/css?family=Roboto+Condensed:300,400");
-@import url("https://fonts.googleapis.com/css?family=Kaushan+Script:400");
-@import url("https://fonts.googleapis.com/css?family=Nanum+Pen+Scriptanddisplay=swap");
-@import url("https://fonts.googleapis.com/css?family=Permanent+Markeranddisplay=swap");
+@import 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css';
+@import 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.css';
 
-.heading-1 {
-  text-align: center;
-  margin-bottom: 2%;
-  margin-top: 2%;
-  font-size: 3.5em;
-  font-family: "Permanent Marker", cursive;
-  color: rgb(32, 32, 32);
-}
+.profile-card{
+            position:relative;
+            overflow: hidden;
+            margin-bottom:10px;
+        }
+        .profile-card .profile-img img{
+            width:100%;
+            height:auto;
+        }
+        .profile-card .profile-content::before{
+            content: "";
+            position:absolute;
+            top:0;
+            left:0;
+            width:50%;
+            height:40%;
+            border:3px solid #222;
+            border-right:3px solid transparent;
+            border-bottom:3px solid transparent;
+            transform: scale(1,1);
+            transition: all 0.50s linear;
+        }
+        .profile-card .profile-content::after{
+            content: "";
+            position:absolute;
+            bottom:0;
+            right:0;
+            width:50%;
+            height:40%;
+            border:3px solid #222;
+            border-left:3px solid transparent;
+            border-top:3px solid transparent;
+            transition: all 0.50s linear;
+        }
+        .profile-card:hover .profile-content:before{
+            top:20px;
+            left:20px;
+        }
+        .profile-card:hover .profile-content:after{
+            bottom:20px;
+            right:20px;
+        }
+        .profile-card .profile-content::after{
+            content: "";
+            position:absolute;
+            bottom:0;
+            right:0;
+            width:50%;
+            height:40%;
+            border:3px solid #222;
+            border-left:3px solid transparent;
+            border-top:3px solid transparent;
+            transform: scale(1,1);
+            transition: all 0.50s linear;
+        }
+        .profile-card .profile-content{
+            position: absolute;
+            top:0;
+            left:0;
+            width:100%;
+            height:100%;
+            padding:50% 4%;
+            text-align: center;
+            background-color: rgba(255,255,255,0.4);
+        }
+        .profile-card .profile-content .title{
+            font-size:24px;
+            color:#333;
+            opacity:0;
+            letter-spacing:2px;
+            transform: translateY(-100px);
+            transition:all 900ms cubic-bezier(0.68, -0.55, 0.265, 1.55) 0.20s;
+        }
+        .profile-card:hover .title{
+            opacity:1;
+            transform: translateY(0);
+        }
+        .profile-card:hover .title::after{
+            content:"";
+            display:block;
+            width:50%;
+            margin:10px auto;
+            border-bottom:2px solid #222;
+        }
+        .profile-card .profile-content .title > span{
+            display:block;
+            margin:5px 0;
+            font-size: 14px;
+        }
+        .profile-card .profile-content .social-link{
+            margin:20px 0;
+            padding:0;
+            opacity:0;
+            transform: translateY(100px);
+            transition:all 600ms cubic-bezier(0.68, -0.55, 0.265, 1.55) 0.20s;
+        }
+        .profile-card:hover .social-link{
+            opacity:1;
+            transform: translateY(0);
+        }
+        .profile-card .profile-content .social-link li{
+            display: inline-block;
+            list-style: none;
+            margin:0 4px;
+        }
+        .profile-card .profile-content .social-link li a{
+            color:#333;
+            text-align: center;
+        }
+        .fa-github{
+          font-size: 95px;
 
-.heading-2 {
-  text-align: center;
-  margin-bottom: 2%;
-  margin-top: 2%;
-  font-size: 2.5em;
-  background: #151515;
-  /* background: black; */
-  color: white;
-  font-family: "Permanent Marker", cursive;
-  font-weight: 400;
-  padding-top: 0.5%;
-  padding-bottom: 0.5%;
-}
+        }
+        .fa-github:hover{
+          font-size: 100px;
+          color: #6e5494;
+          background-color: rgba(256,256,256,0.5);
+          border: 10px;
+          border-radius: 50px; 
+          transition: all 0.5s;
 
-.image {
-  margin: auto;
-  /* width: 6vw; */
-  /* height: auto; */
-  /* border-radius: 50%; */
-}
+        }
+        .fa-linkedin-in{
+          font-size: 95px;
 
-.card {
-  padding-top: 7%;
-  padding-bottom: 3%;
+        }
+        .fa-linkedin-in:hover{
+          font-size: 100px;
+          color: #0077B5;
+          line-height: 100%;
+          background-color: rgba(256,256,256,0.5);
+          border: 100px;
+          border-radius: 20px; 
+          transition: all 0.5s;
 
-  /* background: linear-gradient(to bottom, #33ccff 0%, #ff99cc 100%); */
-  /* background-color:orange; */
-}
-
-.img-container {
-  padding: auto;
-  image-orientation: from-image;
-}
-
-.contact {
-  padding-left: 5%;
-  padding-right: 5%;
-  margin-bottom: 5%;
-}
-
-.details {
-  margin-top: 5%;
-  margin-bottom: 5%;
-  text-align: center;
-}
-
-.img-1 {
-  /* padding-left: 30%;
-    padding-right: 30%; */
-  text-align: center;
-}
-
-.outside {
-  width: 100%;
-  height: 100vh;
-  background: url("https://firebasestorage.googleapis.com/v0/b/waves-assets.appspot.com/o/Backgrounds%2FBackground_Events-min.jpg?alt=media&token=f73ff590-5812-43e8-b6ee-62cc2cd7e442");
-  padding-left: 0px;
-  padding-right: 0px;
-}
-
-.row {
-  margin-bottom: 3%;
-}
+        }
 </style>
